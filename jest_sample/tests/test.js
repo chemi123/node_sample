dbfca5a6960974232c20dee0a776fd00e2048b7c
Yuichi Tanimoto <chemiec@gmail.com>
2018-10-01 15:05:22 +0000

    add callback test sample

--- jest_sample/tests/test.js
+++ jest_sample/tests/test.js
@@ -20,3 +20,16 @@ describe('describe2', () => {
     expect(util.value).toBe(10);
   });
 });
+
+it('callback test starts', done => {
+  let callback = () => {
+    let a = 10;
+    expect(a).toBe(10);
+
+    // jestの基本仕様はテストの最後の行(この例の場合だとsetTimeoutが呼ばれた時)に終了する
+    // そのため、この例のcallback関数を呼びたいのであればdoneを呼んでおく必要がある
+    done();
+  }
+
+  setTimeout(callback, 1000);
+});
